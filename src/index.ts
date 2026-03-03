7daf8baeaefaef0186c4c26f95533ebd417c25ca
Ryan Jehan <ryan.jehan@aligent.com.au>
2026-03-04 09:53:05 +1030

    Updated the warning message

--- src/index.ts
+++ src/index.ts
@@ -59,7 +59,7 @@ export default class ServerlessConventions implements ServerlessPlugin {
     if (parseFloat(version) < 3) {
       this.log.warning(
         `You are using an old Serverless version (${version}).\n` +
-          `Please consider upgrading Serverless to v4 or downgrade this plugin to v1.0.0`
+          `Please consider upgrading Serverless to v3 or downgrade this plugin to v0.4.1`
       );
     }
   }
